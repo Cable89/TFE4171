c119d4af60b5bc1caf2f5a30954dd72a28d49aba
Eivind Gamst <eivingam@mercury.tele.ntnu.no>
2016-04-27 17:49:33 +0200

    syntax checked on playground

--- trunk/code/top/tb/hdlc_tb.sv
+++ trunk/code/top/tb/hdlc_tb.sv
@@ -1,4 +1,4 @@
-timeunit 10ns;
+meunit 10ns;
 
 `include "hdlc_packet.sv"
 //'include "tb_wrapper.v"
@@ -26,28 +26,20 @@ module hdlc_tb(output
 
 parameter NMESSAGES=10;
 
-//initial begin
-//  clk_i=0;
-//  txclk=0;
-//  rxclk=0;
-//end
-
+bit rx_thingy;
 HDLC_packet test_message[NMESSAGES];
 
 initial
 message_gen: begin
-  clk_i=0;
-  txclk=0;
-  rxclk=0;
   for (int i = 0; i < NMESSAGES; i++) begin
     test_message[i] = new;
-    test_message[i].getbits(rx);
+    test_message[i].getbits(rx_thingy);
   end
 end:message_gen
   
-always #1  clk_i =~clk_i;
-always #25 txclk =~txclk;
-always #25 rxclk =~txclk;
+//always #1  clk_i =~clk_i;
+//always #25 txclk =~txclk;
+//always #25 rxclk =~txclk;
 
 endmodule : hdlc_tb
 
